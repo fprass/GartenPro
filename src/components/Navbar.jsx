f2519f5d6eafb9786e0d0e0801f9d63958a1bdc5
fabian <fabian.prass@gmail.com>
2026-04-15 21:41:59 +0200

    feat: style sidebar with active link

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,16 +4,31 @@ import { NavLink } from "react-router-dom";
 function Navbar() {
   return (
     <>
-      <nav className="flex flex-col items-center fixed left-0 top-0 h-full w-64">
-        <NavLink to="/">
+      <nav className="flex flex-col items-center fixed left-0 top-0 h-full w-64 p-4 bg-green-800 text-white">
+        <NavLink
+          className={({ isActive }) =>
+            `flex items-center gap-2 p-3 rounded-lg ${isActive ? "bg-green-600" : "hover:bg-green-700"}`
+          }
+          to="/"
+        >
           <LayoutDashboard />
           <label>Dashboard</label>
         </NavLink>
-        <NavLink to="/Customer">
+        <NavLink
+          className={({ isActive }) =>
+            `flex items-center gap-2 p-3 rounded-lg ${isActive ? "bg-green-600" : "hover:bg-green-700"}`
+          }
+          to="/Customer"
+        >
           <Users />
           <label>Kunde</label>
         </NavLink>
-        <NavLink to="/Offer">
+        <NavLink
+          className={({ isActive }) =>
+            `flex items-center gap-2 p-3 rounded-lg ${isActive ? "bg-green-600" : "hover:bg-green-700"}`
+          }
+          to="/Offer"
+        >
           <FileText />
           <label>Angebot</label>
         </NavLink>
